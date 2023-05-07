22ffaf460d6e80055d5a13b189f4698988bf018a
Baumann, Marco <marco.baumann@stud.thga.de>
2023-05-07 17:56:16 +0200

    adding format.c

---
+++ format.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int d = 100;	
+  double f = 3.1415926535;
+printf ("%d\n", d);
+printf ("%f\n", f);
+
+  char c = 'A';
+  char s[] = "Hello World!";
+printf ("%c\n", c);
+printf ("%s\n", s);
+
+}
+
+
